eee7bbaccfad303770e319ea5dda4b13dde63964
johan.castiblanco <johan.castiblanco@pevaar.com>
2021-10-19 18:23:13 -0500

    automation using gulp
    library for automation using the `package.json`

---
+++ tools/automation/gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp')
+const server = require('gulp-server-livereload')
+gulp.task('build', function(cb) {
+    console.log('building the site')
+    setTimeout(cb, 1200)
+    cb()
+})
+
+gulp.task('serve', function(cb){
+    gulp.src("www")
+        .pipe(server({
+            livereload: true,
+            open: true
+        }))
+})
+
+gulp.task('default', gulp.series('build', 'serve'))
