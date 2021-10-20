dbb86b15b985215cfd133ffd2f41c2b31bf3c1bc
johan.castiblanco <johan.castiblanco@pevaar.com>
2021-10-20 08:17:12 -0500

    fix fs.js

--- modules/fs.js
+++ modules/fs.js
@@ -16,11 +16,7 @@ function delete_file(path, cb) {
 
 
 function write(path, content, cb) {
-<<<<<<< HEAD
     fs.writeFile(path, content, (err) => { //ASYNC, wait to response. oR is slower.
-=======
-    fs.writeFile(path, content, (err) => { //ASYNC
->>>>>>> 12517511bc221eee36a8330620e33a4d6a8e177c
         if (err) {
             console.err("i cant write it,", error)
         } else {
@@ -41,12 +37,7 @@ delete_file(__dirname + '/file1.txt', console.log)
 
 
 //read(__dirname + '/file1.txt')
-<<<<<<< HEAD
 //write2(__dirname + '/file2.txt', 'I am a new file niiga', console.log)
 //delete_file(__dirname + '/file2.txt', console.log)
 
 //fs function return null when there was no problem.
-=======
-write2(__dirname + '/file2.txt', 'I am a new file niiga', console.log)
-delete_file(__dirname + '/file2.txt', console.log)
->>>>>>> 12517511bc221eee36a8330620e33a4d6a8e177c
